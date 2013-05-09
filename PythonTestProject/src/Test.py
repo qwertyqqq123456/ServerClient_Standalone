652ac8380f6a4c1b82f10adb59b4d4cb113bb6a7
boyusun <boyusun@163.com>
2013-05-09 14:40:57 -0700

    Version 2
    In this version, test code is multithreaded, but in one process.
    When client>500, result is very bad, due to my CPU capacity.
    Need more test....

--- PythonTestProject/src/Test.py
+++ PythonTestProject/src/Test.py
@@ -2,14 +2,16 @@ import socket
 import threading
 import SocketServer
 import time
+from random import randrange
 
 devicelist = {}
+devicenumber_index = {}
 
 class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
     'This handler is for handling request on server'
     def handle(self):
         'This function handles the processing of the requests'
-        data = self.request.recv(1024)
+        data = self.request.recv(128)
         paralist = data.split("#")
         
         if paralist[0] == "R":          
@@ -18,6 +20,10 @@ class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
             connected = False
             isalive = True
             devicelist[device_name] = [pairing_info, connected, isalive]
+            
+            devicenumber = len(devicenumber_index) + 1
+            devicenumber_index[devicenumber] = device_name
+            
             if pairing_info in devicelist:
                 if devicelist[pairing_info][0] == device_name:
                     devicelist[pairing_info][1] = True
@@ -26,43 +32,47 @@ class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
                     print "Some error in devicelist: [name mismatching]"
                 
             print"Current devlist:", devicelist
-            response = "{0} registered" .format(device_name)
+            print"Current devicenumber_index:", devicenumber_index
+            response = "{0} registered as {1}" .format(device_name, devicenumber)
         
         elif paralist[0] == "D":
-            sender_name = paralist[1]
-            code = paralist[2]
+            sender_number = int(paralist[1])
+            code = paralist[2]             
             if code == "Send":
                 color = paralist[3]
                 brightness = paralist[4]
-            
-            if code == "Send":
-                operation = "Send " + color + " " + brightness
-            elif code == "Reply":
-                operation = "Reply"
-            else:
-                print "error code"
+                         
+            if sender_number in devicenumber_index:
+                sender_name = devicenumber_index[sender_number]
+
+                'Send msg'
+                if code == "Send":
+                    operation = "Send " + color + " " + brightness
+                elif code == "Reply":
+                    operation = "Reply"
+                else:
+                    print "error code"
+                    
+                if len(devicelist[devicelist[sender_name][0]]) == 3:
+                    devicelist[devicelist[sender_name][0]].append(operation)
+                elif len(devicelist[devicelist[sender_name][0]]) == 4:
+                    devicelist[devicelist[sender_name][0]][3] = operation
+                else:
+                    print "device record length error"
                 
-            if len(devicelist[devicelist[sender_name][0]]) == 3:
-                devicelist[devicelist[sender_name][0]].append(operation)
-            elif len(devicelist[devicelist[sender_name][0]]) == 4:
-                devicelist[devicelist[sender_name][0]][3] = operation
-            else:
-                print "device record length error"
-            
-            response = "message sent to server"
+                'Response'    
+                if len(devicelist[sender_name]) == 3:
+                    response = "no message this time"
+                elif len(devicelist[sender_name]) == 4:
+                    response = devicelist[sender_name][3]
+                else:
+                    print "device record length error2"
+                response = sender_name + ": " + response
                 
-            
-        elif paralist[0] == "G":
-            sender_name = paralist[1]
-            if len(devicelist[sender_name]) == 3:
-                response = "no message this time"
-            elif len(devicelist[sender_name]) == 4:
-                response = devicelist[sender_name][3]
             else:
-                print "device record length error2"
+                response = "Didn't find this device number in system!"
         else:
-            pass
-            
+            pass          
         
         self.request.sendall(response)
         
@@ -71,12 +81,14 @@ class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
     pass
 
 def client(ip, port, message):
+    randtime = randrange(0, 7)
+    time.sleep(randtime)
     'This function is for executing client logic'
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((ip, port))
     try:
         sock.sendall(message)
-        response = sock.recv(1024)
+        response = sock.recv(128)
         print"Received: {}".format(response)
     finally:
         sock.close()
@@ -95,25 +107,31 @@ if __name__ == "__main__":
     print "Loop...server thread name: ", server_thread.name
     print "\n"
 
-    client(ip, port, "R#" + "Tommy_mac#" + "Amy_windows")
-    client(ip, port, "R#" + "Jeff_mac#" + "Marry_mac")
-    client(ip, port, "R#" + "Amy_windows#" + "Tommy_mac")
-    client(ip, port, "R#" + "Marry_mac#" + "Jeff_mac")
-    client(ip, port, "R#" + "Julie_windows#" + "Frank_windows")
-    client(ip, port, "R#" + "Frank_windows#" + "Julie_windows")
+    totalnumber = 10
+    times = 20
     
-    client(ip, port, "D#" + "Tommy_mac#" + "Send#" + "Red#" + "20")
-    time.sleep(1)
-    client(ip, port, "G#" + "Amy_windows")
+    for i in range(1, (totalnumber / 2) + 1):
+        reg_message = "R#Client{0}#Client{1}".format(i, (i + totalnumber / 2))
+        (threading.Thread(target=client, args=(ip, port, reg_message))).start()
+    for i in range((totalnumber / 2) + 1, totalnumber + 1):
+        reg_message = "R#Client{0}#Client{1}".format(i, (i - totalnumber / 2))
+        (threading.Thread(target=client, args=(ip, port, reg_message))).start()
+
+    print "Waiting for registration to be completed...\n"
+    time.sleep(30)
     
-    time.sleep(3)
-    
-    client(ip, port, "D#" + "Amy_windows#" + "Reply#" + "#" + "") 
-    time.sleep(1)
-    client(ip, port, "G#" + "Tommy_mac")
+    for j in range(times):
+        randnumber_1 = randrange(1, totalnumber + 101)
+        randnumber_2 = randrange(1, totalnumber)
+        if randnumber_1 % 2 == 0:
+            msg = "D#{0}#Send#Red#20".format(randnumber_2) 
+        else:
+            msg = "D#{0}#Reply##".format(randnumber_2)
+        
+        (threading.Thread(target=client, args=(ip, port, msg))).start()
        
-    server.shutdown()
-    print "Server shutdown."
+    """erver.shutdown()
+    print "Server shutdown." """
     
 
 
